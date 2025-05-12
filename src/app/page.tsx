7c7e568969fafecb9ee77456ae4eaf10ee9fb712
Sawaiz8 <sawaiz.nasim@gmail.com>
2025-05-12 22:57:49 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Top />
-      <Documentary />
+      {/* <Documentary /> */}
       <WhoWeAre />
       <Collaborators />
       {/* <Donate /> */}
