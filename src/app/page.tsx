055240541c9e644baede6d4e48802751f889fef3
Sawaiz8 <sawaiz.nasim@gmail.com>
2025-05-29 23:30:35 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import WhoWeAre from "../components/PageComponents/Home/who-we-are";
 import Top from "../components/PageComponents/Home/top";
 import FoundationFacts from "../components/PageComponents/Home/foundationFacts";
 import "./globals.css"; // Adjust the path if your CSS file is located elsewhere
-// import Documentary from "./home-components/documentary";
+import Documentary from "../components/PageComponents/Home/documentary";
 
 export default function Home() {
   return (
     <main>
       <Top />
-      {/* <Documentary /> */}
+      <Documentary />
       <WhoWeAre />
       <Collaborators />
       {/* <Donate /> */}
