6f57306d6fff9db482a6afd79c443f87c451047b
Sawaiz8 <sawaiz.nasim@gmail.com>
2025-08-31 00:24:39 +0500

    removed component

--- src/app/what-we-do/page.tsx
+++ src/app/what-we-do/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import FoundationFacts from "../../components/PageComponents/Home/foundationFacts";
+// import FoundationFacts from "../../components/PageComponents/Home/foundationFacts";
 import WeDo from "../../components/PageComponents/WhatWeDo/we-do";
 
 function WhatWeDo() {
   return (
     <>
       <WeDo />
-      <FoundationFacts />
+      {/* <FoundationFacts /> */}
     </>
   );
 }
