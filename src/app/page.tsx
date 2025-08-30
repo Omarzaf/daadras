f60b54258910ee2f6ae91ef049c09f05bc813460
Sawaiz8 <sawaiz.nasim@gmail.com>
2025-08-30 22:12:31 +0500

    added doc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Collaborators from "../components/PageComponents/Home/collaborators";
 import WhoWeAre from "../components/PageComponents/Home/who-we-are";
 import Top from "../components/PageComponents/Home/top";
 import FoundationFacts from "../components/PageComponents/Home/foundationFacts";
+import Documentary from "../components/PageComponents/Home/documentary";
 // import FeaturedNews from "../components/PageComponents/Home/FeaturedNews";
 import "./globals.css"; // Adjust the path if your CSS file is located elsewhere
 
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <main>
       <Top />
+      <Documentary />
       <WhoWeAre />
       <Collaborators />
       <FoundationFacts />
