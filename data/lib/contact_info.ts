27a8acfbe367911adb1b1d22bf77f15335c68d35
Sawaiz8 <sawaiz.nasim@gmail.com>
2025-09-13 08:54:33 +0500

    updated

--- data/lib/contact_info.ts
+++
@@ -1,5 +0,0 @@
-export const DAADRAS_CONTACT = {
-  email: "daadrashr@gmail.com",
-  phone: "+92 339 0322301",
-  officeHours: "Monday - Sunday: 9:00 AM - 10:00 PM",
-}
--- data/lib/socialMedia.ts
+++
@@ -1,7 +0,0 @@
-export const SOCIAL_MEDIA_LINKS = {
-  linkedin: "https://www.linkedin.com/in/daadras-ngo-25962826b/",
-  instagram: "https://www.instagram.com/daadrasfoundation/",
-  facebook: "",
-  twitter: "",
-  youtube: ""
-};
