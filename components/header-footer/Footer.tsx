3c673d0ed70068836a654d87639007573620eba5
Abdul Wahhab <s.wahhab@outlook.com>
2025-09-17 11:14:09 +0500

    Fixed footer responsiveness and Prettify the Initiative Section

--- components/header-footer/Footer.tsx
+++ components/header-footer/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
   return (
     <footer className="shadow-[0_-3px_3px_-1px_rgba(17,30,71,0.15)] bg-[#FFFFFF] py-12">
       <div className="max-w-[1380px] w-full mx-auto px-6 md:px-10 lg:px-14">
-        <div className="grid md:grid-cols-4 gap-8">
+        <div className="grid gap-4 md:grid-cols-2 md:gap-10">
           <div>
             <div className="flex items-center gap-4 mb-4">
               <img
@@ -23,56 +23,58 @@ export const Footer = () => {
             </p>
             <SocialLinks />
           </div>
-          <div>
-            <h4 className="font-semibold mb-4 text-primary">Programs</h4>
-            <ul className="space-y-2 text-muted-foreground">
-              {Programs.map((item, index) => (
-                <li key={`programs-list-${index}`}>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8">
+            <div>
+              <h4 className="font-semibold mb-4 text-primary">Programs</h4>
+              <ul className="space-y-2 text-muted-foreground">
+                {Programs.map((item, index) => (
+                  <li key={`programs-list-${index}`}>
+                    <Link
+                      href={item.link}
+                      className="hover:text-accent transition-colors"
+                    >
+                      {item.text}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h4 className="font-semibold mb-4 text-primary">Get Involved</h4>
+              <ul className="space-y-2 text-muted-foreground">
+                {GetInvolved.map((item, index) => (
+                  <li key={`getInvolved-list-${index}`}>
+                    <Link
+                      href={item.link}
+                      className="hover:text-accent transition-colors"
+                    >
+                      {item.text}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="col-span-2 md:col-span-1">
+              <h4 className="font-semibold mb-4 text-primary">Contact</h4>
+              <ul className="space-y-2 text-muted-foreground">
+                <li>
                   <Link
-                    href={item.link}
+                    href={`mailto:${DAADRAS_CONTACT.email}`}
                     className="hover:text-accent transition-colors"
                   >
-                    {item.text}
+                    {DAADRAS_CONTACT.email}
                   </Link>
                 </li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <h4 className="font-semibold mb-4 text-primary">Get Involved</h4>
-            <ul className="space-y-2 text-muted-foreground">
-              {GetInvolved.map((item, index) => (
-                <li key={`getInvolved-list-${index}`}>
+                <li>
                   <Link
-                    href={item.link}
+                    href={`tel:${DAADRAS_CONTACT.phone.replace(/[^+\d]/g, "")}`}
                     className="hover:text-accent transition-colors"
                   >
-                    {item.text}
+                    {DAADRAS_CONTACT.phone}
                   </Link>
                 </li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <h4 className="font-semibold mb-4 text-primary">Contact</h4>
-            <ul className="space-y-2 text-muted-foreground">
-              <li>
-                <Link
-                  href={`mailto:${DAADRAS_CONTACT.email}`}
-                  className="hover:text-accent transition-colors"
-                >
-                  {DAADRAS_CONTACT.email}
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href={`tel:${DAADRAS_CONTACT.phone.replace(/[^+\d]/g, "")}`}
-                  className="hover:text-accent transition-colors"
-                >
-                  {DAADRAS_CONTACT.phone}
-                </Link>
-              </li>
-            </ul>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
