3481fc5ea9f1c15b0b4ee8dd084b6482a28d0d7d
Sawaiz8 <sawaiz.nasim@gmail.com>
2025-09-27 23:17:18 +0500

    updated

--- app/initiatives/project-salam/sel-program/page.tsx
+++ app/initiatives/project-salam/sel-program/page.tsx
@@ -17,7 +17,7 @@ export default function SELProgramPage() {
       <CurriculumSection />
       <VolunteerSection />
       <ChallengesSection />
-      <ImpactSection />
+      {/* <ImpactSection /> */}
     </div>
   )
 }
